5f346367112842000451bcd4d050f23750c3f712
murugesan <murugesan.ponnusamy@natwest.com>
2025-04-09 17:40:38 +0530

    3rd push

--- s3.tf
+++ s3.tf
@@ -1,14 +1,14 @@
 resource "aws_s3_bucket" "b1" {
-    bucket = "muru-123456789"
+    bucket = "muru10-123456789"
 }
 
 resource "aws_s3_bucket" "b2" {
-    bucket = "muru1-123456789"
+    bucket = "muru20-123456789"
 }
 
 resource "aws_s3_bucket" "b3" {
-    bucket = "muru2-123456789"
+    bucket = "muru30-123456789"
 }
 resource "aws_s3_bucket" "b4" {
-    bucket = "muru3-123456789"
+    bucket = "muru40-123456789"
 }
